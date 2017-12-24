0826ba84d7e102ffaa713b83e70d9db3c386dcc6
Nick <nicholas.fung@alumni.ubc.ca>
2017-12-24 22:00:47 +0800

    Major Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,17 @@
-function scrolly(element){
-    var defaultDuration = 500;
-    var edgeOffset = 30;
-    var myDiv = document.getElementById("container");
-    var myScroller = zenscroll.createScroller(myDiv, defaultDuration, edgeOffset);
-    var target = document.getElementById(element);
-    myScroller.center(target);
+// Scroll Duration in milliseconds
+const scrollDuration = 500;
+$('button').addClass('hvr-sink');
+
+function scrolly(targetElement){
+    var container = document.getElementById("container");
+    var containScroller = zenscroll.createScroller(container, scrollDuration, 0);
+    var target = document.getElementById(targetElement);
+    var buttons = document.getElementsByTagName("buttons");
+     
+    // Stops the button for a while
+    $(':button').prop('disabled', true);
+    containScroller.center(target, scrollDuration, 0, function(){
+        $(':button').prop('disabled', false);
+    });
+
 }
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-sticky *= 1.25;
-var container = document.getElementById("container");
-
-$('#container').bind('mousewheel DOMMouseScroll', function (e) { return false; });
-
-$('#container').scroll(function(){
-
-    //    console.log("Curr. Y offset: " + container.scrollTop);
-    //   console.log("Sticky: " + sticky);
-    if (container.scrollTop >= sticky) {
-        navbar.classList.add("sticky");
-    }
-    else{
-        navbar.classList.remove("sticky");
-    }
-    return false;
-});
