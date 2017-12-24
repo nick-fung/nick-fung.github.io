92f4e2a8059e2e05030da2b7af176ddabffcbff8
Nick <nicholas.fung@alumni.ubc.ca>
2017-12-24 10:01:55 +0800

    Finally got it workinggit add -u

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,28 @@
 function scrolly(element){
     var defaultDuration = 500;
     var edgeOffset = 30;
-    var myDiv = document.getElementById("body");
+    var myDiv = document.getElementById("container");
     var myScroller = zenscroll.createScroller(myDiv, defaultDuration, edgeOffset);
     var target = document.getElementById(element);
     myScroller.center(target);
 }
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
 
-// Get the navbar
 var navbar = document.getElementById("navbar");
-var main = document.getElementById("main");
-
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
+sticky *= 1.25;
+var container = document.getElementById("container");
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    console.log(window.pageYOffset);
-  if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-  } else {
-      navbar.classList.remove("sticky");
-  }
-}
+//$('#container').bind('mousewheel DOMMouseScroll', function (e) { return false; });
+
+$('#container').scroll(function(){
+
+    //    console.log("Curr. Y offset: " + container.scrollTop);
+    //   console.log("Sticky: " + sticky);
+    if (container.scrollTop >= sticky) {
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+    return false;
+});
